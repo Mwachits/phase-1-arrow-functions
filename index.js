0154c9a2e96d930446d5907374e454a46a6eef5f
David Karauni <davymwachits@gmail.com>
2024-07-12 17:37:58 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+// Function expression - divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function - square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function - add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Export functions if needed (depending on your setup)
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
